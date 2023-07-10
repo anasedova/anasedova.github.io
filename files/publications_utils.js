e6192d900575728ead6cd80747f96495672d93ac
agsedova <anastasiia.sedova@gmail.com>
2023-07-11 00:27:55 +0200

    bugfix

--- files/publications_utils.js
+++ files/publications_utils.js
@@ -16,7 +16,7 @@ function addAbstract(abstract, paper_name) {
 }
 
 var actc_abstract = 'Self-supervised knowledge-graph completion (KGC) relies on estimating a scoring model over ' +
-    '(entity, relation, entity)-tuples, for exam- ple, by embedding an initial knowledge graph. Prediction quality can ' +
+    '(entity, relation, entity)-tuples, for example, by embedding an initial knowledge graph. Prediction quality can ' +
     'be improved by calibrating the scoring model, typically by adjusting the prediction thresholds using manually ' +
     'annotated examples. In this paper, we attempt for the first time cold-start calibration for KGC, where no annotated' +
     'examples exist initially for calibration, and only a limited number of tuples can be selected for annotation.' +
@@ -39,21 +39,21 @@ var actc_citation = '@inproceedings{sedova-roth-2023-actc,' +
     'pages = "1853--1863"}';
 
 var actc_citation_text = '@inproceedings{sedova-roth-2023-actc, \n ' +
-    ' &emsp; title = "{ACTC}: Active Threshold Calibration for Cold-Start Knowledge Graph Completion",\n ' +
-    ' &emsp; author = "Sedova, Anastasiia  and Roth, Benjamin", \n' +
-    ' &emsp; booktitle = "Proceedings of the 61st Annual Meeting of the Association for Computational Linguistics (Volume 2: Short Papers)",\n' +
-    ' &emsp; month = jul,\n' +
-    ' &emsp; year = "2023",\n' +
-    ' &emsp; address = "Toronto, Canada",\n' +
-    ' &emsp; publisher = "Association for Computational Linguistics",\n' +
-    ' &emsp; url = "https://aclanthology.org/2023.acl-short.158",\n' +
-    ' &emsp; pages = "1853--1863"}';
+    ' \t title = "{ACTC}: Active Threshold Calibration for Cold-Start Knowledge Graph Completion",\n ' +
+    ' \t author = "Sedova, Anastasiia  and Roth, Benjamin", \n' +
+    ' \t booktitle = "Proceedings of the 61st Annual Meeting of the Association for Computational Linguistics (Volume 2: Short Papers)",\n' +
+    ' \t month = jul,\n' +
+    ' \t year = "2023",\n' +
+    ' \t address = "Toronto, Canada",\n' +
+    ' \t publisher = "Association for Computational Linguistics",\n' +
+    ' \t url = "https://aclanthology.org/2023.acl-short.158",\n' +
+    ' \t pages = "1853--1863"}';
 
 var agra_abstract = "An accurate and substantial dataset is essential for training a reliable and " +
     "well-performing model. However, even manually annotated datasets contain label errors, not to mention " +
     "automatically labeled ones. Previous methods for label denoising have primarily focused on detecting outliers and " +
     "their permanent removal – a process that is likely to over- or underfilter the dataset. In this work, we propose " +
-    "AGRA: a new method for learning with noisy labels by using Adaptive GRAdient-based outlier removal1. Instead of " +
+    "AGRA: a new method for learning with noisy labels by using Adaptive GRAdient-based outlier removal. Instead of " +
     "cleaning the dataset prior to model training, the dataset is dynamically adjusted during the training process. " +
     "By comparing the aggregated gradient of a batch of samples and an individual example gradient, our method " +
     "dynamically decides whether a corresponding example is helpful for the model at this point or is " +
@@ -70,12 +70,12 @@ var agra_citation = "@misc{sedova2023learning," +
     "primaryClass={cs.LG}}";
 
 var agra_citation_text = "@misc{sedova2023learning,\n" +
-    " &emsp; title={Learning with Noisy Labels by Adaptive Gradient-Based Outlier Removal}, \n" +
-    " &emsp; author={Anastasiia Sedova and Lena Zellinger and Benjamin Roth},\n" +
-    " &emsp; year={2023},\n" +
-    " &emsp; eprint={2306.04502},\n" +
-    " &emsp; archivePrefix={arXiv},\n" +
-    " &emsp; primaryClass={cs.LG} \n }";
+    " \t title={Learning with Noisy Labels by Adaptive Gradient-Based Outlier Removal}, \n" +
+    " \t author={Anastasiia Sedova and Lena Zellinger and Benjamin Roth},\n" +
+    " \t year={2023},\n" +
+    " \t eprint={2306.04502},\n" +
+    " \t archivePrefix={arXiv},\n" +
+    " \t primaryClass={cs.LG} \n }";
 
 var knodle_abstract = "Strategies for improving the training and prediction quality of weakly supervised " +
     "machine learning models vary in how much they are tailored to a specific task or integrated with a specific model " +
@@ -100,14 +100,14 @@ var knodle_citation = "@inproceedings{DBLP:conf/rep4nlp/SedovaSSR21," +
     "doi={10.18653/v1/2021.repl4nlp-1.12}}";
 
 var knodle_citation_text = "@inproceedings{DBLP:conf/rep4nlp/SedovaSSR21,  \n" +
-    " &emsp; author={Anastasiia Sedova and Andreas Stephan and Marina Speranskaya and Benjamin Roth}, \n" +
-    " &emsp; title={Knodle: Modular Weakly Supervised Learning with PyTorch},\n" +
-    " &emsp; booktitle={Proceedings of the 6th Workshop on Representation Learning for NLP, RepL4NLP@ACL-IJCNLP 2021, Online, August 6, 2021},\n" +
-    " &emsp; pages={100--111},\n " +
-    " &emsp; publisher={Association for Computational Linguistics},\n" +
-    " &emsp; year={2021},\n" +
-    " &emsp; url={https://doi.org/10.18653/v1/2021.repl4nlp-1.12},\n" +
-    " &emsp; doi={10.18653/v1/2021.repl4nlp-1.12} \n }";
+    " \t author={Anastasiia Sedova and Andreas Stephan and Marina Speranskaya and Benjamin Roth}, \n" +
+    " \t title={Knodle: Modular Weakly Supervised Learning with PyTorch},\n" +
+    " \t booktitle={Proceedings of the 6th Workshop on Representation Learning for NLP, RepL4NLP@ACL-IJCNLP 2021, Online, August 6, 2021},\n" +
+    " \t pages={100--111},\n " +
+    " \t publisher={Association for Computational Linguistics},\n" +
+    " \t year={2021},\n" +
+    " \t url={https://doi.org/10.18653/v1/2021.repl4nlp-1.12},\n" +
+    " \t doi={10.18653/v1/2021.repl4nlp-1.12} \n }";
 
 var topic_modeling_lemmata_citation = "@inproceedings{DBLP:conf/ims2/SedovaM17," +
     "author= {Anastasiya Sedova and Olga Mitrofanova}," +
@@ -123,18 +123,18 @@ var topic_modeling_lemmata_citation = "@inproceedings{DBLP:conf/ims2/SedovaM17,"
     "bibsource={dblp computer science bibliography, https://dblp.org}}";
 
 var topic_modeling_lemmata_citation_text = "@inproceedings{DBLP:conf/ims2/SedovaM17, \n" +
-    " &emsp; author= {Anastasiya Sedova and Olga Mitrofanova}, \n" +
-    " &emsp; title= {Topic Modelling of Russian Texts based on Lemmata and Lexical Constructions}, \n" +
-    " &emsp; booktitle= {XX International Conference 'Internet and Modern Society': Computer" +
-    " &emsp; Linguistics and Computing Ontologies, {IMS} {CLCO} 2017, St. Petersburg, Russia, June 21-23, 2017}, \n" +
-    " &emsp; pages={132--144}, \n" +
-    " &emsp; publisher={ITMO University}, \n" +
-    " &emsp; year={2017}, \n" +
-    " &emsp; url={https://doi.org/10.17586/2541-9781-2017-1-132-144}, \n" +
-    " &emsp; doi={10.17586/2541-9781-2017-1-132-144}, \n" +
-    " &emsp; timestamp={Wed, 10 Mar 2021 10:49:24 +0100},\n" +
-    " &emsp; biburl={https://dblp.org/rec/conf/ims2/SedovaM17.bib},\n" +
-    " &emsp; bibsource={dblp computer science bibliography, https://dblp.org} \n }";
+    " \t author= {Anastasiya Sedova and Olga Mitrofanova}, \n" +
+    " \t title= {Topic Modelling of Russian Texts based on Lemmata and Lexical Constructions}, \n" +
+    " \t booktitle= {XX International Conference 'Internet and Modern Society': Computer" +
+    " \t Linguistics and Computing Ontologies, {IMS} {CLCO} 2017, St. Petersburg, Russia, June 21-23, 2017}, \n" +
+    " \t pages={132--144}, \n" +
+    " \t publisher={ITMO University}, \n" +
+    " \t year={2017}, \n" +
+    " \t url={https://doi.org/10.17586/2541-9781-2017-1-132-144}, \n" +
+    " \t doi={10.17586/2541-9781-2017-1-132-144}, \n" +
+    " \t timestamp={Wed, 10 Mar 2021 10:49:24 +0100},\n" +
+    " \t biburl={https://dblp.org/rec/conf/ims2/SedovaM17.bib},\n" +
+    " \t bibsource={dblp computer science bibliography, https://dblp.org} \n }";
 
 var topic_modeling_comparable_abstract = "The paper is devoted to processing parallel and comparable corpora by " +
     "means of topic modelling. We focus our attention on Russian and English parallel and comparable texts. We use " +
@@ -162,17 +162,17 @@ var topic_modeling_comparable_citation = "@inproceedings{10.1145/3143699.3143734
     "series = {IMS2017}}";
 
 var topic_modeling_comparable_citation_text = "@inproceedings{10.1145/3143699.3143734, \n" +
-    " &emsp; author = {Mitrofanova, Olga and Sedova, Anastasiia}, \n" +
-    " &emsp; title = {Topic Modelling in Parallel and Comparable Fiction Texts (the Case Study of English and Russian Prose)}, \n" +
-    " &emsp; year = {2017}, \n" +
-    " &emsp; isbn = {9781450354370}, \n" +
-    " &emsp; publisher = {Association for Computing Machinery}, \n" +
-    " &emsp; address = {New York, NY, USA}, \n" +
-    " &emsp; url = {https://doi.org/10.1145/3143699.3143734}, \n" +
-    " &emsp; doi = {10.1145/3143699.3143734}, \n" +
-    " &emsp; booktitle = {Proceedings of the International Conference IMS-2017},  \n" +
-    " &emsp; pages = {175–180},  \n" +
-    " &emsp; numpages = {6}, \n" +
-    " &emsp; keywords = {Topic Modelling, Fiction, Text Corpora, English, Russian, Parallel and Comparable Texts}, \n" +
-    " &emsp; location = {Saint Petersburg, Russian Federation}, \n" +
-    " &emsp; series = {IMS2017} \n }";
+    " \t author = {Mitrofanova, Olga and Sedova, Anastasiia}, \n" +
+    " \t title = {Topic Modelling in Parallel and Comparable Fiction Texts (the Case Study of English and Russian Prose)}, \n" +
+    " \t year = {2017}, \n" +
+    " \t isbn = {9781450354370}, \n" +
+    " \t publisher = {Association for Computing Machinery}, \n" +
+    " \t address = {New York, NY, USA}, \n" +
+    " \t url = {https://doi.org/10.1145/3143699.3143734}, \n" +
+    " \t doi = {10.1145/3143699.3143734}, \n" +
+    " \t booktitle = {Proceedings of the International Conference IMS-2017},  \n" +
+    " \t pages = {175–180},  \n" +
+    " \t numpages = {6}, \n" +
+    " \t keywords = {Topic Modelling, Fiction, Text Corpora, English, Russian, Parallel and Comparable Texts}, \n" +
+    " \t location = {Saint Petersburg, Russian Federation}, \n" +
+    " \t series = {IMS2017} \n }";
